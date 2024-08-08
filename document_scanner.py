44f2b945d2f698315d7bdb0cdeb707ad9880ea20
sandesh bhantana <sandeshbhantana@gmail.com>
2024-08-08 15:17:13 +0545

    [add: new scripts]
    [four point transformation and document scanner]

---
+++ document_scanner.py
@@ -0,0 +1,64 @@
+# To use simply run
+"""python document_scanner.py -i hello.png"""
+
+import imutils
+import argparse
+import cv2 as cv
+import numpy as np
+from transform import four_point_transform
+from skimage.filters import threshold_local
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-i", "--image", required=True, help="Path to the image to be scanned")
+args = vars(ap.parse_args())
+
+
+image = cv.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height=500)
+
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+gray = cv.GaussianBlur(gray, (5, 5), 0)
+edged = cv.Canny(gray, 75, 200)
+
+print("Step 1: Edge Detection")
+cv.imshow("Image", image)
+cv.imshow("Edged", edged)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+cnts = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key=cv.contourArea, reverse=True)[:5]
+
+screenCnt = np.array([])  # defining empty matlike first
+
+for con in cnts:
+    peri = cv.arcLength(con, True)
+    approx = cv.approxPolyDP(con, 0.02 * peri, True)
+
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+print("Step 2: Find contours of paper")
+cv.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv.imshow("Outline", image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+warped = cv.cvtColor(warped, cv.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset=10, method="gaussian")
+warped = (warped > T).astype("uint8") * 255
+
+
+print("Step 3: Apply perspective transform")
+cv.imshow("Original", imutils.resize(orig, height=650))
+cv.imshow("Warped", imutils.resize(warped, height=650))
+cv.waitKey(0)
+cv.destroyAllWindows()
---
+++ transform_example.py
@@ -0,0 +1,28 @@
+# run this in command line as
+""" python transform_example.py --image <path-to-image> --coords "[(top-left x and y), (top-right x and y), (bottom-right x and y), (bottom-left x and y)]"
+"""
+
+from transform import four_point_transform
+import numpy as np
+import cv2 as cv
+import argparse
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help="path to the image file")
+ap.add_argument("-c", "--coords", help="comma seperated list of source points")
+args = vars(ap.parse_args())
+
+image = cv.imread(args["image"])
+pts = np.array(eval(args["coords"]), dtype="float32")
+
+warped = four_point_transform(image, pts)
+
+cv.imshow("Original", image)
+cv.imshow("Warped", warped)
+cv.waitKey(0)
+
+
+# to test with this repo's hello.png the command will be,
+""" python transform_example.py --image hello.png --coords "[(73, 239), (356, 117), (475, 265), (187, 443)]"
+"""
