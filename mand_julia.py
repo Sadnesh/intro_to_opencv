db280e0a0abb98b487b9cd0fac0eb6a060555621
sandesh bhantana <sandeshbhantana@gmail.com>
2024-08-16 00:25:12 +0545

    [update: mand_julia script with ways to display the fractal]

--- mand_julia.py
+++ mand_julia.py
@@ -5,7 +5,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 # the most abstracted method
-# Image.effect_mandelbrot((4000, 4000), (-3, -2.5, 2, 2.5), 100).show()
+Image.effect_mandelbrot((4000, 4000), (-3, -2.5, 2, 2.5), 100).show()
 
 
 # function to yield both mandelbrot and julia sets
@@ -29,5 +29,65 @@ julia_gen = julia(1, 0.25)
 
 for _ in range(10):
     print(next(mand_gen))
+
 for _ in range(10):
     print(next(julia_gen))
+
+
+# plotting using matplotlib
+def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
+    # for:
+    # real = np.array([1, 2, 3])
+    # imaginary = np.array([4, 5])
+    # the output from this function will be: array([[1.+4.j, 2.+4.j, 3.+4.j],[1.+5.j, 2.+5.j, 3.+5.j]])
+
+    real = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+    imaginary = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
+    return real[np.newaxis, :] + imaginary[:, np.newaxis] * 1j
+
+
+def is_stable(c, num_iterations) -> bool:
+    z = 0
+    for _ in range(num_iterations):
+        z = z**2 + c
+    return abs(z) <= 2  # threshold of 2 is used generally so
+
+
+def get_members(c, num_iterations):
+    mask = is_stable(c, num_iterations)
+    return c[mask]
+
+
+def show_scatter_plot():
+    c = complex_matrix(-2, 0.5, -1.5, 1.5, 21)
+    members = get_members(c, 20)
+    plt.scatter(members.real, members.imag, color="black", marker=",", s=1)
+    plt.gca().set_aspect("equal")
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+
+def show_blackNwhite_image():
+    c = complex_matrix(-2, 0.5, -1.5, 1.5, 512)
+    plt.imshow(is_stable(c, num_iterations=20), cmap="binary")
+    plt.gca().set_aspect("equal")
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+
+def show_using_pillow():
+    c = complex_matrix(-2, 0.5, -1.5, 1.5, 512)
+    image = Image.fromarray(~is_stable(c, num_iterations=20))  # type:ignore
+    image.show()
+
+
+def main() -> None:
+    show_scatter_plot()
+    show_blackNwhite_image()
+    show_using_pillow()
+
+
+if __name__ == "__main__":
+    main()
