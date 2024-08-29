e4ee60dbde8ea5619eb003d90b2af7455ba4bc2d
sandesh bhantana <sandeshbhantana@gmail.com>
2024-08-29 22:10:19 +0545

    [change: border size and displayed og image]

--- basic_operations_on_image.py
+++ basic_operations_on_image.py
@@ -30,6 +30,7 @@ upper_blue = np.array([255, 70, 70])
 
 mask = cv.inRange(img, lower_blue, upper_blue)
 
+display_image("Original Image", img)
 # select all the white areas of the mask and change to green
 img[mask != 0] = [0, 255, 0]
 
@@ -53,7 +54,7 @@ img[288:340, 150:200] = ball
 display_image("cropped and pasted", img)
 
 # we can split this way as well
-# b, g, r = cv.split(img)
+b, g, r = cv.split(img)
 
 # using only one will make the image appear any of the described color otherwise it will appear black
 img[:, :, 0] = 0  # to make blue pixels to zero (image will appear a bit yellowish)
@@ -62,24 +63,24 @@ img[:, :, 2] = 0  # to make red pixels to zero (image will appear a bit cyan-ish
 
 # and this way we can merge
 # img = cv.merge((b, g, r))
-# display_image("merged", img)
+display_image("merged", img)
 
-# data = 10
-# img = cv.imread("images/letter_j.png")
-# replicate = cv.copyMakeBorder(img, data, data, data, data, cv.BORDER_REPLICATE)
-# reflect = cv.copyMakeBorder(img, data, data, data, data, cv.BORDER_REFLECT)
-# reflect101 = cv.copyMakeBorder(img, data, data, data, data, cv.BORDER_REFLECT_101)
-# wrap = cv.copyMakeBorder(img, data, data, data, data, cv.BORDER_WRAP)
-# constant = cv.copyMakeBorder(
-#     img, data, data, data, data, cv.BORDER_CONSTANT, value=(255, 0, 0)
-# )
-# plt.subplot(231), plt.imshow(img, "gray"), plt.title("ORIGINAL")  # type:ignore
-# plt.subplot(232), plt.imshow(replicate, "gray"), plt.title("REPLICATE")  # type:ignore
-# plt.subplot(233), plt.imshow(reflect, "gray"), plt.title("REFLECT")  # type:ignore
-# plt.subplot(234), plt.imshow(reflect101, "gray"), plt.title(
-#     "REFLECT_101"
-# )  # type:ignore
-# plt.subplot(235), plt.imshow(wrap, "gray"), plt.title("WARP")  # type:ignore
-# plt.subplot(236), plt.imshow(constant, "gray"), plt.title("CONSTANT")  # type:ignore
+data = 20
+img = cv.imread("images/letter_j.png")
+replicate = cv.copyMakeBorder(img, data, data, data, data, cv.BORDER_REPLICATE)
+reflect = cv.copyMakeBorder(img, data, data, data, data, cv.BORDER_REFLECT)
+reflect101 = cv.copyMakeBorder(img, data, data, data, data, cv.BORDER_REFLECT_101)
+wrap = cv.copyMakeBorder(img, data, data, data, data, cv.BORDER_WRAP)
+constant = cv.copyMakeBorder(
+    img, data, data, data, data, cv.BORDER_CONSTANT, value=(255, 0, 0)
+)
+plt.subplot(231), plt.imshow(img, "gray"), plt.title("ORIGINAL")  # type:ignore
+plt.subplot(232), plt.imshow(replicate, "gray"), plt.title("REPLICATE")  # type:ignore
+plt.subplot(233), plt.imshow(reflect, "gray"), plt.title("REFLECT")  # type:ignore
+plt.subplot(234), plt.imshow(reflect101, "gray"), plt.title(
+    "REFLECT_101"
+)  # type:ignore
+plt.subplot(235), plt.imshow(wrap, "gray"), plt.title("WARP")  # type:ignore
+plt.subplot(236), plt.imshow(constant, "gray"), plt.title("CONSTANT")  # type:ignore
 
-# plt.show()
+plt.show()
