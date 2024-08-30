c15568ddd65c7e61622117446dc9994338e9c602
sandesh bhantana <sandeshbhantana@gmail.com>
2024-08-31 00:02:15 +0545

    [add: probablistic HoughLines]

--- line_detector.py
+++ line_detector.py
@@ -23,3 +23,17 @@ for line in lines:
 cv.imshow("drawn lines", img)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+
+img = cv.imread("images/sudoku.png")
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+edges = cv.Canny(gray, 50, 150, apertureSize=3)
+lines = cv.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=10)
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+
+cv.imshow("New drawn lines", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
